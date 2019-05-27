0f13c977bb9b5e49dea0169d76a3339793487706
7stud <7stud@excite.com>
2019-05-26 22:52:49 -0600

    Initial commit.

---
+++ lib/hello2_web/channels/room_channel.ex
@@ -0,0 +1,26 @@
+defmodule Hello2Web.RoomChannel do
+  use Phoenix.Channel
+  alias Hello2Web.Presence
+
+  def join("room:lobby", _params, socket) do
+    send(self(), :after_join)
+    {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    push(socket, "presence_state", Presence.list(socket))
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:second))
+    })
+    {:noreply, socket}
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (room:lobby).
+
+  # Add authorization logic here as required.
+
+end
